de677a20472091d9188500e546716a8063ad5700
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2021-07-02 15:08:19 +0900

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+echo "installing vim-plug..."
+git clone https://github.com/junegunn/vim-plug.git
+cp vim-plug/plug.vim ~/.vim/autoload/
+rm -rf vim-plug
+echo "making symbolic link..."
 ln -s ~/dotfiles/.vimrc ~/.vimrc
