eeb23736d285c6685e7d7c683dda0cbe7bc3d3d4
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2021-07-15 11:19:46 +0900

    fix string

--- install.bat
+++ install.bat
@@ -5,13 +5,13 @@
 @rem -----------------------------------
 
 @rem setting for XDG_CONFIG_HOME
-setx XDG_CONFIG_HOME "%userprofile%\.config"
+setx XDG_CONFIG_HOME "%USERPROFILE%\.config"
 mkdir %XDG_CONFIG_HOME%\nvim
 
 @rem make symbolic link
 if exist %XDG_CONFIG_HOME%\nvim\init.vim del ^
     %XDG_CONFIG_HOME%\nvim\init.vim
-mklink %XDG_CONFIG_HOME%\nvim\init.vim %userprofile%\dotfiles\init.vim
+mklink %XDG_CONFIG_HOME%\nvim\init.vim %USERPROFILE%\dotfiles\init.vim
 
 @rem install vim-plug
 curl -fo %XDG_CONFIG_HOME%\nvim\autoload\plug.vim --create-dirs ^
