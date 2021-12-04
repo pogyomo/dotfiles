2c57de243968da450b079a701277f0632eba5a19
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2021-12-04 11:59:12 +0900

    recover

--- neovim/lua/config.lua
+++ neovim/lua/config.lua
@@ -3,7 +3,6 @@ require('init.mapping')
 require('init.editor')
 
 -- load plugin settings
-require('plugins.external.vim-plug')
-require('plugins.external.molokai')
-require('plugins.external.hop')
-require('plugins.internal.statusline')
+require('plugins.vim-plug')
+require('plugins.molokai')
+require('plugins.hop')
