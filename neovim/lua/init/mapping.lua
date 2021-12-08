4aa1af164304ac398ae1739d32fe223ffc472cbe
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2021-12-08 22:13:08 +0900

    fix mappings

--- neovim/lua/init/mapping.lua
+++ neovim/lua/init/mapping.lua
@@ -2,34 +2,25 @@
 -- Variable for config
 -- --------------------
 -- Option
-local opts = { noremap = true, silent = true }
-local expr = { noremap = true, silent = true, expr = true }
+local default    = { noremap = true, silent = true }
+local silent_off = { noremap = true, silent = false }
 
 
 -- --------------------
 -- Config of leader key
 -- --------------------
--- Use space key as leader
+-- Use space key as basic leader
 vim.g.mapleader = ' '
 -- Leader for basic key mapping
-vim.api.nvim_set_keymap('n', '<Leader>w', '[Window]',   {})
-vim.api.nvim_set_keymap('n', '<Leader>t', '[Tab]',      {})
-vim.api.nvim_set_keymap('n', '<Leader>s', '[Split]',    {})
--- Leader for plugin key mapping
-vim.api.nvim_set_keymap('n', '<Leader>h', '[Hop]',      {})
-vim.api.nvim_set_keymap('n', '<Leader>g', '[Fugitive]', {})
-vim.api.nvim_set_keymap('n', '<Leader>f', '[Floaterm]', {})
+vim.api.nvim_set_keymap('n', '<Leader>w', '[Window]',    {})
+vim.api.nvim_set_keymap('n', '<Leader>t', '[Tab]',       {})
+vim.api.nvim_set_keymap('n', '<Leader>s', '[Split]',     {})
 
--- Use s key as leader
+-- Use s key as plugin leader
 vim.g.mapleader = 's'
--- Leader for basic key mapping
-vim.api.nvim_set_keymap('n', '<Leader>w', '[Window]',   {})
-vim.api.nvim_set_keymap('n', '<Leader>t', '[Tab]',      {})
-vim.api.nvim_set_keymap('n', '<Leader>s', '[Split]',    {})
 -- Leader for plugin key mapping
-vim.api.nvim_set_keymap('n', '<Leader>h', '[Hop]',      {})
-vim.api.nvim_set_keymap('n', '<Leader>g', '[Fugitive]', {})
-vim.api.nvim_set_keymap('n', '<Leader>f', '[Floaterm]', {})
+vim.api.nvim_set_keymap('n', '<Leader>h', '[Hop]',       {})
+vim.api.nvim_set_keymap('n', '<Leader>f', '[Floaterm]',  {})
 
 
 -- ---------------------------------
@@ -37,88 +28,88 @@ vim.api.nvim_set_keymap('n', '<Leader>f', '[Floaterm]', {})
 -- ---------------------------------
 -- Tab page
 -- Open or close file in tab page
-vim.api.nvim_set_keymap('n', '[Tab]o',  ':tabnew<Space>',  opts)
-vim.api.nvim_set_keymap('n', '[Tab]co', ':tabclose<CR>',   opts)
-vim.api.nvim_set_keymap('n', '[Tab]ca', ':tabonly<CR>',    opts)
+vim.api.nvim_set_keymap('n', '[Tab]n', ':tabnew<Space>',   silent_off)
+vim.api.nvim_set_keymap('n', '[Tab]c', ':tabclose<CR>',    default)
+vim.api.nvim_set_keymap('n', '[Tab]C', ':tabonly<CR>',     default)
 -- Move between tabes
-vim.api.nvim_set_keymap('n', '[Tab]l',  'gt',              opts)
-vim.api.nvim_set_keymap('n', '[Tab]h',  'gT',              opts)
+vim.api.nvim_set_keymap('n', '[Tab]l',  'gt',              default)
+vim.api.nvim_set_keymap('n', '[Tab]h',  'gT',              default)
 -- Move tabe page
-vim.api.nvim_set_keymap('n', '[Tab]L',  ':+tabmove<CR>',   opts)
-vim.api.nvim_set_keymap('n', '[Tab]H',  ':-tabmove<CR>',   opts)
+vim.api.nvim_set_keymap('n', '[Tab]L',  ':+tabmove<CR>',   default)
+vim.api.nvim_set_keymap('n', '[Tab]H',  ':-tabmove<CR>',   default)
 
 -- Window
 -- Move between windows
-vim.api.nvim_set_keymap('n', '[Window]j', '<C-w>j',        opts)
-vim.api.nvim_set_keymap('n', '[Window]k', '<C-w>k',        opts)
-vim.api.nvim_set_keymap('n', '[Window]h', '<C-w>h',        opts)
-vim.api.nvim_set_keymap('n', '[Window]l', '<C-w>l',        opts)
-vim.api.nvim_set_keymap('n', '[Window]J', '<C-w>J',        opts)
-vim.api.nvim_set_keymap('n', '[Window]K', '<C-w>K',        opts)
-vim.api.nvim_set_keymap('n', '[Window]H', '<C-w>H',        opts)
-vim.api.nvim_set_keymap('n', '[Window]L', '<C-w>L',        opts)
+vim.api.nvim_set_keymap('n', '[Window]j', '<C-w>j',        default)
+vim.api.nvim_set_keymap('n', '[Window]k', '<C-w>k',        default)
+vim.api.nvim_set_keymap('n', '[Window]h', '<C-w>h',        default)
+vim.api.nvim_set_keymap('n', '[Window]l', '<C-w>l',        default)
+vim.api.nvim_set_keymap('n', '[Window]J', '<C-w>J',        default)
+vim.api.nvim_set_keymap('n', '[Window]K', '<C-w>K',        default)
+vim.api.nvim_set_keymap('n', '[Window]H', '<C-w>H',        default)
+vim.api.nvim_set_keymap('n', '[Window]L', '<C-w>L',        default)
 -- Change window width and height
-vim.api.nvim_set_keymap('n', '[Window]<', '10<C-w><',      opts)
-vim.api.nvim_set_keymap('n', '[Window]>', '10<C-w>>',      opts)
-vim.api.nvim_set_keymap('n', '[Window]+', '10<C-w>+',      opts)
-vim.api.nvim_set_keymap('n', '[Window]-', '10<C-w>-',      opts)
+vim.api.nvim_set_keymap('n', '[Window]<', '10<C-w><',      default)
+vim.api.nvim_set_keymap('n', '[Window]>', '10<C-w>>',      default)
+vim.api.nvim_set_keymap('n', '[Window]+', '10<C-w>+',      default)
+vim.api.nvim_set_keymap('n', '[Window]-', '10<C-w>-',      default)
 
 -- Split window
-vim.api.nvim_set_keymap('n', '[Split]j', ':sp<CR><C-w>j',  opts)
-vim.api.nvim_set_keymap('n', '[Split]k', ':sp<CR><C-w>k',  opts)
-vim.api.nvim_set_keymap('n', '[Split]h', ':vsp<CR><C-w>h', opts)
-vim.api.nvim_set_keymap('n', '[Split]l', ':vsp<CR><C-w>l', opts)
+vim.api.nvim_set_keymap('n', '[Split]j', ':sp<CR><C-w>j',  default)
+vim.api.nvim_set_keymap('n', '[Split]k', ':sp<CR><C-w>k',  default)
+vim.api.nvim_set_keymap('n', '[Split]h', ':vsp<CR><C-w>h', default)
+vim.api.nvim_set_keymap('n', '[Split]l', ':vsp<CR><C-w>l', default)
 
 -- Move as displayed row
-vim.api.nvim_set_keymap('n', 'j',        'gj',             opts)
-vim.api.nvim_set_keymap('n', 'k',        'gk',             opts)
-vim.api.nvim_set_keymap('n', 'gj',       'j',              opts)
-vim.api.nvim_set_keymap('n', 'gk',       'k',              opts)
+vim.api.nvim_set_keymap('n', 'j',        'gj',             default)
+vim.api.nvim_set_keymap('n', 'k',        'gk',             default)
+vim.api.nvim_set_keymap('n', 'gj',       'j',              default)
+vim.api.nvim_set_keymap('n', 'gk',       'k',              default)
 
 
 -- ----------------------------------
 -- Plugin key mapping for normal mode
 -- ----------------------------------
 -- hop.nvim
-vim.api.nvim_set_keymap('n', '[Hop]w',      ':HopWord<CR>',                   opts)
-vim.api.nvim_set_keymap('n', '[Hop]p',      ':HopPattern<CR>',                opts)
-vim.api.nvim_set_keymap('n', '[Hop]c',      ':HopChar1<CR>',                  opts)
-vim.api.nvim_set_keymap('n', '[Hop]C',      ':HopChar2<CR>',                  opts)
-vim.api.nvim_set_keymap('n', '[Hop]l',      ':HopLine<CR>',                   opts)
-vim.api.nvim_set_keymap('n', '[Hop]L',      ':HopLineStart<CR>',              opts)
+vim.api.nvim_set_keymap('n', '[Hop]w',      ':HopWord<CR>',                   default)
+vim.api.nvim_set_keymap('n', '[Hop]p',      ':HopPattern<CR>',                default)
+vim.api.nvim_set_keymap('n', '[Hop]c',      ':HopChar1<CR>',                  default)
+vim.api.nvim_set_keymap('n', '[Hop]C',      ':HopChar2<CR>',                  default)
+vim.api.nvim_set_keymap('n', '[Hop]l',      ':HopLine<CR>',                   default)
+vim.api.nvim_set_keymap('n', '[Hop]L',      ':HopLineStart<CR>',              default)
 
 -- vim-floaterm
-vim.api.nvim_set_keymap('n', '[Floaterm]n', [[:FloatermNew<CR><C-\><C-n>]],   opts)
-vim.api.nvim_set_keymap('n', '[Floaterm]h', [[:FloatermPrev<CR><C-\><C-n>]],  opts)
-vim.api.nvim_set_keymap('n', '[Floaterm]H', [[:FloatermFirst<CR><C-\><C-n>]], opts)
-vim.api.nvim_set_keymap('n', '[Floaterm]l', [[:FloatermNext<CR><C-\><C-n>]],  opts)
-vim.api.nvim_set_keymap('n', '[Floaterm]L', [[:FloatermLast<CR><C-\><C-n>]],  opts)
-vim.api.nvim_set_keymap('n', '[Floaterm]s', [[:FloatermShow<CR><C-\><C-n>]],  opts)
-vim.api.nvim_set_keymap('n', '[Floaterm]S', [[:FloatermHide<CR><C-\><C-n>]],  opts)
+vim.api.nvim_set_keymap('n', '[Floaterm]n', [[:FloatermNew<CR><C-\><C-n>]],   default)
+vim.api.nvim_set_keymap('n', '[Floaterm]h', [[:FloatermPrev<CR><C-\><C-n>]],  default)
+vim.api.nvim_set_keymap('n', '[Floaterm]H', [[:FloatermFirst<CR><C-\><C-n>]], default)
+vim.api.nvim_set_keymap('n', '[Floaterm]l', [[:FloatermNext<CR><C-\><C-n>]],  default)
+vim.api.nvim_set_keymap('n', '[Floaterm]L', [[:FloatermLast<CR><C-\><C-n>]],  default)
+vim.api.nvim_set_keymap('n', '[Floaterm]s', [[:FloatermShow<CR><C-\><C-n>]],  default)
+vim.api.nvim_set_keymap('n', '[Floaterm]S', [[:FloatermHide<CR><C-\><C-n>]],  default)
 
 
 -- ---------------------------------
 -- Basic key mapping for insert mode
 -- ---------------------------------
 -- Use jj to go to normal mode
-vim.api.nvim_set_keymap('i', 'jj', '<ESC>',                        opts)
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>',                        default)
 
 -- Automatically complete brackets and quotation
-vim.api.nvim_set_keymap('i', '{',   '{}<Left>',                    opts)
-vim.api.nvim_set_keymap('i', '[',   '[]<Left>',                    opts)
-vim.api.nvim_set_keymap('i', '(',   '()<Left>',                    opts)
-vim.api.nvim_set_keymap('i', [[']], [[''<Left>]],                  opts)
-vim.api.nvim_set_keymap('i', [["]], [[""<Left>]],                  opts)
+vim.api.nvim_set_keymap('i', '{',   '{}<Left>',                    default)
+vim.api.nvim_set_keymap('i', '[',   '[]<Left>',                    default)
+vim.api.nvim_set_keymap('i', '(',   '()<Left>',                    default)
+vim.api.nvim_set_keymap('i', [[']], [[''<Left>]],                  default)
+vim.api.nvim_set_keymap('i', [["]], [[""<Left>]],                  default)
 
 -- Make brackets feel good
-vim.api.nvim_set_keymap('i', '{<Enter>', '{}<Left><CR><ESC><S-o>', opts)
-vim.api.nvim_set_keymap('i', '[<Enter>', '[]<Left><CR><ESC><S-o>', opts)
-vim.api.nvim_set_keymap('i', '(<Enter>', '()<Left><CR><ESC><S-o>', opts)
+vim.api.nvim_set_keymap('i', '{<Enter>', '{}<Left><CR><ESC><S-o>', default)
+vim.api.nvim_set_keymap('i', '[<Enter>', '[]<Left><CR><ESC><S-o>', default)
+vim.api.nvim_set_keymap('i', '(<Enter>', '()<Left><CR><ESC><S-o>', default)
 
 
 -- -----------------------------------
 -- Basic key mapping for terminal mode
 -- -----------------------------------
 -- Use Alt-(jj) and Alt-ESC to go to normal mode
-vim.api.nvim_set_keymap('t', '<A-j><A-j>', [[<C-\><C-n>]], opts)
-vim.api.nvim_set_keymap('t', '<A-ESC>',    [[<C-\><C-n>]], opts)
+vim.api.nvim_set_keymap('t', '<A-j><A-j>', [[<C-\><C-n>]], default)
+vim.api.nvim_set_keymap('t', '<A-ESC>',    [[<C-\><C-n>]], default)
--- neovim/lua/plugins/packer.lua
+++ neovim/lua/plugins/packer.lua
@@ -71,7 +71,7 @@ return require('packer').startup(function()
             vim.g.floaterm_width     = 0.8
             vim.g.floaterm_height    = 0.8
             vim.g.floaterm_autoclose = 2
-        end
+        end,
     }
 
     -- Lsp and related plugin
