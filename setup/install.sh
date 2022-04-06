c72c836e4dce534a5bfb9c1d23bf72e8cf68b16d
pogyomo <82942315+pogyomo@users.noreply.github.com>
2022-04-06 20:40:27 +0900

    Add settings of vim

--- setup/install.sh
+++ setup/install.sh
@@ -1,10 +1,3 @@
-## for neovim
-### install vim-plug
-#curl -fo ~/.config/nvim/autoload/plug.vim --create-dirs \
-#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-### make symbolic link
-#ln -sfv ~/dotfiles/neovim/init.vim ~/.config/nvim/init.vim
-
 # for neovim
 ## install packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
@@ -14,3 +7,7 @@ mkdir ~/.config/
 mkdir ~/.config/nvim/
 ln -sfv ~/dotfiles/neovim/init.lua ~/.config/nvim/init.lua
 ln -sfv ~/dotfiles/neovim/lua ~/.config/nvim/lua
+
+# for vim
+## make symbolic link
+ln -sfv ~/dotfiles/vim/init.vim ~/.vimrc
