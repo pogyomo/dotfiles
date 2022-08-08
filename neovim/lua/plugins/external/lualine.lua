39f1be040291a55c662670bd25d9998a14e87042
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2022-08-09 08:51:12 +0900

    Modify statusline

--- neovim/lua/plugins/external/lualine.lua
+++ neovim/lua/plugins/external/lualine.lua
@@ -1,15 +1,32 @@
 local M = {}
 
+function M._lower(str)
+    return string.lower(str)
+end
+
+function M._currtime()
+    return vim.fn.strftime(' %H:%M:%S')
+end
+
 function M.setup()
     local lualine = require('lualine')
 
-    local filename_symbols   = { modified = ' +',     readonly = ' -',    unnamed  = 'no name' }
-    local fileformat_symbols = { unix     = ' unix', dos      = ' dos', mac      = ' mac'   }
+    local filename_symbols   = {
+        modified = ' +',
+        readonly = ' -',
+        unnamed  = 'no name',
+        newfile  = 'new',
+    }
+    local fileformat_symbols = {
+        unix = ' unix',
+        dos  = ' dos',
+        mac  = ' mac',
+    }
 
     local status_line = {
-        lualine_a = { { 'mode', fmt = function(str) return string.lower(str) end } },
+        lualine_a = { { 'mode',  fmt = M._lower } },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', symbols = filename_symbols } },
+        lualine_c = { { 'filename', newfile_status = true, symbols = filename_symbols } },
         lualine_x = { 'encoding', { 'fileformat', symbols = fileformat_symbols }, 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { '%l/%L:%c' }
@@ -20,12 +37,21 @@ function M.setup()
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
-        lualine_y = { function() return vim.fn.strftime(' %H:%M:%S') end },
+        lualine_y = { M._currtime },
         lualine_z = {},
     }
 
+    local status_bar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+    }
+
     lualine.setup {
-        options          = {
+        options = {
             theme = 'sonokai',
             globalstatus = true,
             refresh = {
@@ -37,6 +63,8 @@ function M.setup()
         sections         = status_line,
         inactive_section = status_line,
         tabline          = status_tab,
+        winbar           = status_bar,
+        inactive_winbar  = status_bar,
     }
 end
 
