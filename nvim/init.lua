de6128d18d66767b4b53aa69f4ddec3970667242
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2022-08-19 18:37:24 +0900

    Delete comment

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,2 @@
 require("core")
 require("plugins")
-
---local buf = vim.api.nvim_create_buf(true, true)
---local win = vim.api.nvim_open_win(buf, false, {
---    relative = "editor",
---    row = 30,
---    col = 20,
---    width = 60,
---    height = 20,
---    border = "rounded"
---})
---vim.api.nvim_win_set_option(win, "number", false)
---vim.api.nvim_buf_call(buf, function()
---    vim.fn.termopen(os.getenv("SHELL"))
---end)
