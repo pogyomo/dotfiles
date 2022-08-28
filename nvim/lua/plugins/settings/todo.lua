4a9a1033b7d8d6741806c7fc5e1c7b09781e93d0
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2022-08-28 18:24:31 +0900

    Highlight REVIEW: and CHANGED:

--- nvim/lua/plugins/settings/todo.lua
+++ nvim/lua/plugins/settings/todo.lua
@@ -1,7 +1,12 @@
 local function setup()
     local util = require("utils")
     local mods = util.requires("todo-comments")
-    mods["todo-comments"].setup()
+    mods["todo-comments"].setup{
+        keywords = {
+            REVIEW  = { icon = " ", color = "info" },
+            CHANGED = { icon = " ", color = "hint" },
+        }
+    }
 end
 
 return setup
